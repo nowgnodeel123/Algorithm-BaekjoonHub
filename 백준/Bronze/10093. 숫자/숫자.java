a3618444f5bdffd36c91a4b5f94b3d0085b76c42
LEE DONG WON <168517856+nowgnodeel123@users.noreply.github.com>
2024-09-03 16:59:20 +0900

    [Bronze II] Title: 숫자, Time: 252 ms, Memory: 23264 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        if (a > b) {
+            long tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if (a == b) {
+            sb.append(b - a).append("\n");
+        } else {
+            sb.append(b - a - 1).append("\n");
+        }
+        for (long i = a + 1; i < b; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.print(sb);
+    }
+}

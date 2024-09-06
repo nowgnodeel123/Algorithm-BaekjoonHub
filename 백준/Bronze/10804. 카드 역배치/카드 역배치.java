133b68561ef05fe293ffba2a5af505f0d0b1564a
LEE DONG WON <168517856+nowgnodeel123@users.noreply.github.com>
2024-09-06 19:24:05 +0900

    [Bronze II] Title: 카드 역배치, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] card = new int[21];
+
+        for(int i=0; i<card.length; i++) {
+            card[i] += i;
+        }
+
+        for(int i=0; i<10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int temp = 0;
+
+            for(int j=0; j<(b-a+1)/2; j++) {
+                temp = card[a+j];
+                card[a+j] = card[b-j];
+                card[b-j] = temp;
+            }
+        }
+        br.close();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<card.length; i++) {
+            sb.append(card[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
